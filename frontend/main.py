02ec8782b7f9dedfe48d8e06d0bee096c7be1a56
SAMER MOHAMED ZAID <157971075+SamerZaid20@users.noreply.github.com>
2025-12-06 15:02:30 +0200

    Update main.py

--- frontend/main.py
+++ frontend/main.py
@@ -156,3 +156,23 @@ if st.button("Generate Audio"):
             st.audio(audio_bytes, format="audio/wav")
         else:
             st.error("Error: " + response.text)
+
+from app.controllers.video_controller import VideoController
+
+DATASET = "dataset/asl_alphabet"
+
+controller = VideoController(DATASET)
+
+st.title("ASL Word to Video API")
+st.write("Enter a word to convert it into ASL sign letters video.")
+
+word = st.text_input("Enter a word")
+
+if st.button("Generate Video"):
+    output_path = controller.generate_word_video(word)
+
+    if output_path:
+        st.video(output_path)
+    else:
+        st.error("Failed: No video generated. Check the word or dataset folders.")
+
